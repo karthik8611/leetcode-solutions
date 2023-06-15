5910f7a43f3d519bfb32b0542f7953fc6e1c22c0
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-06-15 11:29:27 +0530

    Time: 1 ms, Memory: 40 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,38 +1,28 @@
 class Solution {
     public int mySqrt(int x) {
+   
+        if(x==0)return 0;
         
-       if(x==0)
-           return 0;
+    int start=1,end=x,ans=0;
         
-        int start=1, end=x;
-        
-        while(start<end){
+        while(start <= end){
+            int mid=start+(end-start)/2;
             
-          int mid=start+(end-start)/2;  
-            
-            if(mid <= x/mid && (mid+1) > x/(mid+1))
-                return mid;
-            else if( mid > x/mid)
-                end =mid;
-            else
-                start =mid+1;
-            
-            
-            
+            if(mid==x/mid) {
+                 return mid;
+            }
+               
+            else if(mid>x/mid) {
+                end=mid-1;
+            } 
+            else{
+                
+                start=mid+1;
+                ans=mid;
+                
+            }//ans is updated during the binary search to keep track of the largest integer mid whose square is less than or equal to x. It acts as a temporary storage to hold the current valid square root candidate until the binary search process completes.
         }
-        return start;
+        return ans;
         
     }
 }
-// if (x == 0) return 0;
-// 	int start = 1, end = x;
-// 	while (start < end) { 
-// 		int mid = start + (end - start) / 2;
-// 		if (mid <= x / mid && (mid + 1) > x / (mid + 1))// Found the result
-// 			return mid; 
-// 		else if (mid > x / mid)// Keep checking the left part
-// 			end = mid;
-// 		else
-// 			start = mid + 1;// Keep checking the right part
-// 	}
-// 	return start;
