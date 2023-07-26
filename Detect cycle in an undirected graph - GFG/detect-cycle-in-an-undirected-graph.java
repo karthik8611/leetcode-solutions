163e5433016466a30a14e02f70be5dd9df3f01f4
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-07-26 22:57:53 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,64 +31,41 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Node{
-    
-    int first;
-    int second;
-    public Node(int first,int second){
-        this.first=first;
-        this.second=second;
-    }
-}
-
 
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         
-        boolean visited [] = new boolean[V];
-        Arrays.fill(visited, false);
+        boolean visited [] = new boolean [V];
+        
         
         for(int i=0;i<V;i++){
+            
             if(visited[i]==false){
-                if(checkCycle(i,adj,visited)){
-                    return true;
-                }
-                
+                if(dfs(i,-1,visited,adj)==true) return true;
             }
+            
         }
+        
+        return false;
+        // Code here
+    }
+    
+    public boolean dfs(int node, int parent ,boolean visited [], ArrayList<ArrayList<Integer>> adj){
+        
+        visited[node]=true;
+        
+        for(Integer it:adj.get(node)){
+            if(visited[it]==false){
+                if(dfs(it,node,visited,adj)== true) return true;
+                
+            } 
+            else if(it!=parent) return true;
+        }
+        
         return false;
         
-    }
-    
-    public boolean checkCycle(int s,ArrayList<ArrayList<Integer>> adj,boolean visited []){
         
-        Queue<Node> queue= new LinkedList<>();
-        
-        queue.add(new Node(s,-1));
-        visited[s]=true;
-        
-        while(!queue.isEmpty()){
-            int node = queue.peek().first;
-            int par=queue.peek().second;
-            queue.remove();
-            
-            
-            for(Integer it: adj.get(node)){
-                if(visited[it]==false){
-                    queue.add(new Node(it,node));
-                    visited[it]=true;
-                
-                }
-                
-                else if(par!=it) return true;
-            }
-            
-            
-        }
-        
-        return  false;
         
     }
-    
 }
