0c991800b4733cde4d29ba6442991f036506ef97
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-08-15 19:25:08 +0530

    Time: 56 ms (39.63%), Space: 56.8 MB (22.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -2,8 +2,9 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
         
        // use leetcode solution for better unerstanding
+    // in this  problem we are using min heap
         
-        PriorityQueue<Integer>pq= new PriorityQueue<>();
+        PriorityQueue<Integer>pq= new PriorityQueue<>();// this is min heap by default smallest elemest will at the front of the queue;
         for(int num:nums){
             pq.offer(num);
             if(pq.size()>k){
@@ -11,6 +12,10 @@ class Solution {
             }
         }
         
+        
+        // using max heap time complexity --> O(n+klogn)  o(n) -->initially we had array so we are building heap out of it--> for building heap +  klogn time for heapify process
+        
+       // using   min heap time complexity --> O(n+(n-k+1)logn(n-k+1))
         return pq.peek();
         
     }
