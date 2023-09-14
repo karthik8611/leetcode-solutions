129656d70374dece09cae97f08c1cc67b9dd1df3
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-09-14 21:12:49 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args)
+	 {
+	  
+	  //taking input using Scanner class
+	  Scanner sc = new Scanner(System.in);
+	  
+	  //taking count of testcases
+	  int t = sc.nextInt();
+	  while(t-- > 0){
+	      
+	      //taking count of houses
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      
+	      //inserting money present in 
+	      //each house in the array
+	      for(int i = 0; i<n; ++i)
+	           arr[i] = sc.nextInt();
+  	      
+  	      //calling method FindMaxSum() of class solve
+  	      System.out.println(new Solution().FindMaxSum(arr, n));
+	 }
+   }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum money the thief can get.
+    public int FindMaxSum(int arr[], int n)
+    {
+        
+        int dp [] = new int[n];
+        
+        Arrays.fill(dp,-1);
+        int index=n-1;
+        return solve(index,arr,dp);
+    }
+    
+    public int solve(int index, int arr[], int dp []){
+        
+        if(index<0) return 0;
+        
+        if(index==0) return arr[index];
+       
+         if(dp[index]!=-1){
+             return dp[index];
+         }
+         
+        
+        int pick= solve(index-2,arr,dp)+arr[index];
+        int nonpick=solve(index-1, arr,dp);
+        
+      return  dp[index]= Math.max(pick,nonpick);
+        
+    }
+    
+    
+}
