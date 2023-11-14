b042b8fc58f70648b7dd2141e2fc49bf11e2ab94
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-11-14 08:36:28 +0530

    Time: 232 ms (43.38%), Space: 44.5 MB (49.02%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        
+        Set<Character> letters= new HashSet<>();
+        
+        
+        int ans=0;
+        
+        for(Character c:s.toCharArray()){
+            letters.add(c);
+            
+        }
+        
+        
+        for(Character letter:letters){
+            int i=-1;
+            int j=0;
+            
+            for(int  k=0;k<s.length();k++){
+                if(s.charAt(k)==letter){
+                    if(i==-1){
+                        i=k;
+                    }
+                    
+                    j=k;
+                }
+            }
+            
+            
+            Set<Character> between = new HashSet<>();
+            
+            for(int k=i+1;k<j;k++){
+            between.add(s.charAt(k));
+            }
+            
+            ans+=between.size();
+        }
+        
+        return ans;
+        
+        
+    
+    }
+}
