ffdcd10b2a79416f0d91197c4a8d223193526e43
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-11-20 23:36:44 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (60.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,23 +15,26 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
         List<List<Integer>> list = new ArrayList<>();
-        levelWise(root,0,list);
+        levelWise(root,list,0);
         return list;
-        
         
     }
     
-    public void levelWise(TreeNode root, int level,  List<List<Integer>> list){
-        if(root==null) return;
+    public void levelWise(TreeNode root,List<List<Integer>> list,int level){
+        
+        if(root==null)return;
+        
         
         if(list.size()==level){
             list.add(new ArrayList<>());
-          }
-        list.get(level).add(root.val);
-         levelWise(root.left,level+1,list);
-        levelWise(root.right,level+1,list);
-    
+         }
+         list.get(level).add(root.val);
+        
+        
+        levelWise(root.left,list,level+1);
+         levelWise(root.right,list,level+1);
+        
+        
     }
 }
