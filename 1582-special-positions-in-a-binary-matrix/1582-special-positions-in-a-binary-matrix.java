175934648cf1299b9ce5a286789c293262477db8
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-12-13 10:59:32 +0530

    Time: 1 ms (99.75%), Space: 43.9 MB (47.47%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -3,32 +3,30 @@ class Solution {
         
         int m=mat.length;
         int n=mat[0].length;
-        int count=0;
+        int[] rowCount= new int[m];
+        int[] colCount=new int[n];
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0)continue;
-                
-                boolean good=true;
-                
-                for(int r=0;r<m;r++){
-                    if(mat[r][j]==1 && r!=i){
-                        good=false;
-                    break;
-                    }
+                if(mat[i][j]==1){
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
-                
-                for(int c=0;c<n;c++){
-                    if(mat[i][c]==1 && c!=j){good=false;
-                    break;
-                                            }
-                }
-                
-                if(good)count++;
             }
         }
         
-        return count;
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    if(rowCount[i]==1 && colCount[j]==1){
+                        ans++;
+                    }
+                }
+            }
+        }
+        
+        return ans;
         
     }
 }
