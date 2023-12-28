509b278f475c5b8de7948716d3afb7d4cee7b773
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2023-12-28 13:40:45 +0530

    Time: 4 ms (11.55%), Space: 42.6 MB (14.66%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,54 +1,43 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         
-        if(grid==null || grid.length==0) return 0;
-        
         int rows=grid.length;
         int cols=grid[0].length;
-        Queue<int []> q= new LinkedList<>();
-        
-        int count_fresh=0;
+        int fresh_count=0;
+        Queue<int[]> queue = new LinkedList<>();
         
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(grid[i][j]==2){
-                    q.offer(new int[] {i,j});// put  rotten mangoes in the queue
+                    queue.offer(new int[]{i,j});
                 }else if(grid[i][j]==1){
-                    count_fresh++;
+                    fresh_count++;
+              }
+            }
+        }
+        
+        int count=0;
+        int[][] dirs={{1,0},{-1,0},{0,1},{0,-1}};
+        while(!queue.isEmpty() && fresh_count>0 ){
+            count++;
+            int size=queue.size();
+            
+            for(int i=0;i<size;i++){
+                int[] point=queue.poll();
+                
+                for(int[] dir:dirs){
+                    int x=point[0]+dir[0];
+                    int y=point[1]+dir[1];
+                    
+                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)continue;
+                    grid[x][y]=2;
+                    fresh_count--;
+                    queue.add(new int[]{x,y});
                 }
             }
         }
         
-        if(count_fresh==0)return 0;
-        int count=0;
+        return fresh_count==0?count:-1;
         
-        int [][] dirs ={{1,0},{-1,0},{0,1},{0,-1}}; // 4 directions
-        
-        while(!q.isEmpty() && count_fresh>0){
-          
-            count++;
-            int size =q.size();
-           
-            
-            for(int i=0;i<size;i++){
-                
-                int [] point = q.poll();
-            for(int dir []:dirs){
-                 
-                int x= point[0]+dir[0];
-                int y=point[1]+dir[1];
-                
-                if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)continue;
-                
-                grid[x][y]=2;
-                
-                q.offer(new int [] {x,y});
-                count_fresh--;
-                } 
-            }
-        }
-        
-        return count_fresh==0?count: -1;
-        
-        }
+    }
 }
