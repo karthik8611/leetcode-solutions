8d39bbb8f159efb2d1cd029377cc9fcaac1391de
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2024-01-29 13:27:15 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (73.00%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -18,36 +18,25 @@ class Solution {
         
         List<Integer> ans= new ArrayList<>();
         if(root==null) return ans;
-        List<List<Integer>> list= new ArrayList<>();
-        levelOrder(root,list,0);
+         levelOrder(root,ans,0);
+
         
-//         for(int i=0;i<list.size();i++){
-//             for(int j=0;j<list.get(i).size();j++){
-//                 ans.add(list.get(i).get(list.get(i).size()-1));
-//             }
-//         }
-         for (List<Integer> sublist : list) {
-            int lastIndex = sublist.size() - 1;
-            if (lastIndex >= 0) { // Check if sublist is not empty
-                ans.add(sublist.get(lastIndex));
-            }
-        }
         
         return ans;
         
     }
     
-    public void levelOrder(TreeNode root, List<List<Integer>> list,int level){
+    public void levelOrder(TreeNode root, List<Integer> list,int level){
         if(root==null){
             return;
         }
         
         if(level==list.size()){
-            list.add(new ArrayList<>());
+            list.add(root.val);
         }
-        list.get(level).add(root.val);
-        levelOrder(root.left,list,level+1);
+      
         levelOrder(root.right,list,level+1);
+        levelOrder(root.left,list,level+1);
         
         
     }
