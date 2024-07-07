c8242b659d4085edb5f1cf7a2320de2246f3f162
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2024-07-07 19:41:19 +0530

    Time: 37 ms (50.00%), Space: 123.5 MB (50.00%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,35 +1,41 @@
 class Solution {
-
-    private int[][] gridBlockSum(char[][] grid, char ch) {
-        int n = grid.length, m = grid[0].length;
-        int[][] pre = new int[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                pre[i][j] = (grid[i][j] == ch) ? 1 : 0;
-                if (i > 0) pre[i][j] += pre[i - 1][j];
-                if (j > 0) pre[i][j] += pre[i][j - 1];
-                if (i > 0 && j > 0) pre[i][j] -= pre[i - 1][j - 1];
-            }
-        }
-        
-        return pre;
-    }
-
     public int numberOfSubmatrices(char[][] grid) {
-        int n = grid.length, m = grid[0].length;
         
-        int[][] xCount = gridBlockSum(grid, 'X');
-        int[][] yCount = gridBlockSum(grid, 'Y');
+        int n=grid.length,m=grid[0].length;
         
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (xCount[i][j] == yCount[i][j] && xCount[i][j] > 0) {
+        int[][] xCount= gridSum(grid,'X');
+        int[][] yCount= gridSum(grid,'Y');
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(xCount[i][j]==yCount[i][j] && xCount[i][j]>0){
                     ans++;
                 }
             }
         }
         return ans;
+            
+        
+        
+    }
+    
+    private int[][] gridSum(char[][] grid,char ch){
+        
+        int n=grid.length,m=grid[0].length;
+        
+          int[][] prefix = new int[n][m];
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                prefix[i][j]=grid[i][j]==ch?1:0;
+                
+                if(i>0) prefix[i][j]+=prefix[i-1][j];
+                if(j>0) prefix[i][j]+=prefix[i][j-1];
+                if(i>0 && j>0) prefix[i][j]-=prefix[i-1][j-1];
+            }
+        }
+        
+        return prefix;
     }
 }
