be250a9232109ffa6c9bab55eea481942f7be075
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2024-08-24 22:01:14 +0530

    Time: 1 ms (30.04%), Space: 44.8 MB (8.30%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,17 +14,38 @@
  * }
  */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return height(root)!=-1;
+     class Pair{
+        boolean Balanced;
+        int height;
+        
+        public Pair(boolean Balanced, int height){
+            this.Balanced=Balanced;
+            this.height=height;
+        }
         
     }
-    
-    public int height(TreeNode root){
-        if(root==null) return 0;
-        int l=height(root.left);
-        int r=height(root.right);
+    public Pair helperFun(TreeNode root){
         
-        if(Math.abs(l-r)>1 || r==-1 || l==-1) return -1;
-        return 1+Math.max(l,r);
+        if(root==null){
+            return new Pair(true,0);
+        }
+        
+        Pair lst=helperFun(root.left);
+        Pair rst=helperFun(root.right);
+        
+        int height=Math.max(lst.height,rst.height)+1;
+        
+        boolean isRootBalanced=Math.abs(lst.height-rst.height)<=1;
+        
+        if(lst.Balanced==true && rst.Balanced==true && isRootBalanced==true){
+            return new Pair(true,height);
+        }
+            
+        return new Pair(false,0);
+    }
+    public boolean isBalanced(TreeNode root) {
+        Pair p = helperFun(root);
+        
+        return p.Balanced;
     }
 }
