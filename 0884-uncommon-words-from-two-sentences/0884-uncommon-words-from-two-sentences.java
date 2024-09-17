55912cd2dc99532efdcf5fc603692b7340deb2f3
karthik8611 <109943457+karthik8611@users.noreply.github.com>
2024-09-17 09:53:48 +0530

    Time: 3 ms (38.65%), Space: 42 MB (69.28%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
@@ -1,38 +1,25 @@
 class Solution {
 
     public String[] uncommonFromSentences(String s1, String s2) {
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap<>();
+  
         List<String> ans = new ArrayList<>();
-
-        String[] ss1 = s1.split(" ");
-
-        String[] ss2 = s2.split(" ");
-
-        for (String word : ss1) {
-            map1.put(word, map1.getOrDefault(word,0) + 1);
+        
+        for(String word:s1.split(" ")){
+            countMap.put(word,countMap.getOrDefault(word,0)+1);
         }
         
-       // System.out.println(map1.size());
-
-        for (String word : ss2) {
-            map2.put(word, map2.getOrDefault(word,0) + 1);
+        for(String word:s2.split(" ")){
+            countMap.put(word,countMap.getOrDefault(word,0)+1);
         }
-          //System.out.println(map2.size());
-        for (String s : map1.keySet()) {
-             // System.out.println(map1.get(s));
-            if (!map2.containsKey(s) && map1.get(s) == 1) {
-                ans.add(s);
+        
+        for(String word:countMap.keySet()){
+            if(countMap.get(word)==1){
+                ans.add(word);
             }
         }
 
-        for (String s : map2.keySet()) {
-            if (!map1.containsKey(s) && map2.get(s) == 1) {
-                ans.add(s);
-            }
-        }
-        
-      //  System.out.println(ans.size());
+   
 
         return ans.toArray(new String[0]);
     }
